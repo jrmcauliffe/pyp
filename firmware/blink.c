8992ba7138fc40d5df4fc92ab81096a900d59d03
Jonathan McAuliffe <jon@birchavenue.org>
2020-05-30 14:26:22 +1000

    Basic debounced switch

--- firmware/blink.c
+++ firmware/blink.c
@@ -1,38 +1,75 @@
 #include <msp430.h>				
-
+#include <stdbool.h>
 /**
  * blink.c
  */
 
 
+const unsigned int ticks = 10000;
+const unsigned int intensity_step = 10;
+
+volatile unsigned int intensity;
+volatile unsigned int state;
+
+
 // Simple gamma adjustment
-int gamma(int percentage) {
-    return (181 / 100 ) * percentage * (181/100) * percentage; // calculate gamma using basic squared
+unsigned int gamma(unsigned int percentage) {
+    return percentage * percentage; // calculate gamma using basic squared
 }
 
 void main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;		// stop watchdog timer
-	P1DIR |= 0x40;					// configure P1.6 as output
-    P1SEL |= 0x40;                  // configure P1.6 as special
-	volatile unsigned int i;		// volatile to prevent optimization
-	volatile unsigned int p;
 
+
+    WDTCTL = WDTPW + WDTHOLD;     // Stop WDT
+
+    // Initalise all pins to output to reduce power consumption on unused pins
+    P1DIR = 0xFF;
+    P1OUT = 0x00;
+    P2DIR = 0xFF;
+    P2OUT = 0x00;
+    // LED output
+	P1DIR |= BIT0 + BIT6;			// configure P1.0 and P1.6 as output
+    P1SEL |= BIT6;                  // configure P1.6 as special
+
+    // Pushbutton input
+    P1DIR &= ~BIT3;                // configure P1.3 as input
+    P1REN |= BIT3;                 // Enable internal pull-up/down resistors
+    P1OUT |= BIT3;                 //Select pull-up mode for P1.3
+    P1IE |= BIT3;                   // Enable P1.3 interrupts
+    P1IES |= BIT3;                  // P1.3 Hi/lo edge
+    P1IFG &= ~BIT3;                 // P1.3 IFG cleared
+
+    // Timer A configuration
+    intensity = 0;
 	TACTL = 0x210;
-    TACCR0 = 8000;
-    TACCR1 = 0;
+    TACCR0 = ticks;
+    TACCR1 = gamma(intensity);
     TACCTL0 = 0x90;
     TACCTL1 = 0x10E0;
+    P1OUT &= ~0x01;
+
+    _BIS_SR(CPUOFF + GIE);          // Enter LPM0 w/ interrupt
+
+}
+
+// Timer A0 interrupt service routine
+#pragma vector=TIMERA0_VECTOR
+__interrupt void Timer_A (void)
+{
+    // Debounce button press
+    state = ( state << 1 ) | (P1IN & BIT3)| 0xE000;
+    if(state == 0xF000) {
+        intensity = (intensity + intensity_step) % (100 + intensity_step);
+        TACCR1 = gamma(intensity);
+    }
 
 
-
-	while(1)
-	{
-	    TACCR1 = (TACCR1 % 8000) + 200;
-	    for(p = 0; p<=100; p=p+20){
-            TACCR1 = gamma(p);
-	        for(i=100000; i>0; i--); //delay
-
-	    }
-	}
+  // P1OUT ^= BIT0;                          // Toggle P1.0
+}
+// Port 1 interrupt service routine
+#pragma vector=PORT1_VECTOR
+__interrupt void Port_1(void)
+{
+   P1IFG &= ~BIT3;                     // P1.3 IFG cleared
 }
